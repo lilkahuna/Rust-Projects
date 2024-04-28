4b5a06f86eb36eeb860f93ff96e591249ce0f654
Brodie Liverman <90534314+lilkahuna@users.noreply.github.com>
2024-04-28 02:00:33 -0400

    Delete first_project/target directory

--- first_project/target/debug/deps/first_project-05784b06b590531e.d
+++
@@ -1,5 +0,0 @@
-E:\Projects\Rust-Projects\first_project\target\debug\deps\libfirst_project-05784b06b590531e.rmeta: src\main.rs
-
-E:\Projects\Rust-Projects\first_project\target\debug\deps\first_project-05784b06b590531e.d: src\main.rs
-
-src\main.rs:
