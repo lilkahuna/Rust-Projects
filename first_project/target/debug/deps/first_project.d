929f37233325dc900519896fe7764b6b8a67ffea
lilkahuna <90534314+lilkahuna@users.noreply.github.com>
2024-04-28 01:54:34 -0400

    Learning more concepts

---
+++ first_project/target/debug/deps/first_project.d
@@ -0,0 +1,5 @@
+E:\Projects\Rust-Projects\first_project\target\debug\deps\first_project.exe: src\main.rs
+
+E:\Projects\Rust-Projects\first_project\target\debug\deps\first_project.d: src\main.rs
+
+src\main.rs:
---
+++ first_project/target/debug/first_project.d
@@ -0,0 +1,1 @@
+E:\Projects\Rust-Projects\first_project\target\debug\first_project.exe: E:\Projects\Rust-Projects\first_project\src\main.rs
