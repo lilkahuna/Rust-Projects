979bebdd310e698c79f759559e8d9c9015c83faa
lilkahuna <90534314+lilkahuna@users.noreply.github.com>
2024-06-04 07:54:07 -0400

    additions

---
+++ os/src/main.rs
@@ -0,0 +1,18 @@
+#![no_std]
+#![no_main]
+
+mod vga_buffer;
+use core::panic::PanicInfo;
+
+// no_mange ensures the compiler doesn't change the name of the function
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
